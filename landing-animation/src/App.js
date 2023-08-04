35c0f904fc10e46a408097a1821bd2c9e2a19fbe
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-08-04 20:00:57 +0200

    init landing-animation

---
+++ landing-animation/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <div className='logo_wrapper'>
+          Dev <span>animation</span>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
